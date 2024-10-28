3f04671b3c65c8ef222d8b7f0b1e0437ec2561e8
abr-ya <gamer05@list.ru>
2024-10-28 09:29:58 +0300

    feat: Add json-server for Data

--- src/modules/todo-list/api.ts
+++ src/modules/todo-list/api.ts
@@ -1,15 +1,38 @@
+import axios from "axios";
 import { todoData } from "./todoData";
 
-export type TodoDto = {
+export type TodoDtoV1 = {
   id: string;
   text: string;
   done: boolean;
   userId: string;
 };
 
+export type TodoDto = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: string;
+};
+
+const BASE_URL = "http://localhost:8888";
+
+const baseAxios = axios.create({
+  baseURL: BASE_URL,
+  // baseURL: process.env.API_URL,
+});
+
+// interface ITodoListApi {
+//   getTodoList: () => AxiosResponse<TodoDto[]>;
+// }
+
 export const getTasks = () =>
-  new Promise<TodoDto[]>((res) => {
+  new Promise<TodoDtoV1[]>((res) => {
     setTimeout(() => {
       res(todoData);
     }, 1000);
   });
+
+export const todoListApi = {
+  getTodoList: () => baseAxios.get<TodoDto[]>("/todos"),
+};
--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,18 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
-import { getTasks } from "./api";
+import { todoListApi } from "./api";
 
 export const TodoList = () => {
-  const { data, error, isPending } = useQuery({ queryKey: ["tasks", "list"], queryFn: getTasks });
+  const { data, error, isPending } = useQuery({ queryKey: ["tasks", "list"], queryFn: todoListApi.getTodoList });
 
   if (isPending) return <>loading...</>;
 
   if (error) return <>error: {JSON.stringify(error)}</>;
 
+  console.log(data);
+
+  if (!data) {
+    console.log("Temp line for undefined data! == ToDo!!!");
+
+    return null;
+  }
+
   return (
     <div>
       <h2>Todo List</h2>
-      {data.map((todo) => (
-        <div key={todo.id}>{todo.text}</div>
+      {data.data.map((todo) => (
+        <div key={todo.id}>{todo.title}</div>
       ))}
     </div>
   );
